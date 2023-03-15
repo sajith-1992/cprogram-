3638cbcbc4f4e955c500ceaac2ed4f79eb663e7d
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-15 08:34:28 -0400

    array length in c ooperator should know

---
+++ length array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+#include <stdio.h>
+
+
+const int N=10;
+float average (int length,int array[] );
+int main() {
+   int score[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+
+  
+      printf("%f ", average( N,score));
+         
+     
+      
+   return 0;
+}
+float average (int length,int array[]){
+  int sum =0;
+ for(int i=0;i<length;i++){
+  sum=sum+array[i];
+}
+return sum/(float)N;}
+
