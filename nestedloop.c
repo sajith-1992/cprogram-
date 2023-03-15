6be6e688c7ae16b2afc070110e73beb35945b523
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-14 21:36:12 -0400

    array

---
+++ nestedloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main (void){
+    int n=2;
+ 
+    for(int i=0;i<n;i++){
+        
+         for(int k=0;k<n;k++){
+             printf("hai");
+             
+         }
+         printf("hello\n");
+
+}
+}
