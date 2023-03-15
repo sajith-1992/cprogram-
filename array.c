361125e5344fdda800f2fb2429193df5ccc8f193
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-15 08:23:33 -0400

    array function

--- array.c
+++ array.c
@@ -1,12 +1,26 @@
 
 #include <stdio.h>
 
+
+#include <stdio.h>
+
+const int N=10;
+float average (int array[] );
 int main() {
    int score[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-   int i;
 
-   for(i = 0; i < 10; i++)
-     { printf("%d ", score[i]);}
+
+  
+      printf("%f ", average(score));
+         
+     
       
    return 0;
+}
+float average (int array[]){
+  int sum =0;
+ for(int i=0;i<N;i++){
+  sum=sum+array[i];
+}
+return sum/(float)N;
 }
