719ff932e8deda28b059bb345b48804f4d8142e2
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-16 09:26:52 -0400

    create  command line

---
+++ commandline.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>]
+#include <string.h>
+// need to include sting library 
+
+/// @brief 
+/// @param argc 
+/// @param argv 
+/// @return 
+int main(int argc, string argv[])
+{
+    printf("hekki %c"argv[0]);
+
+    return 0;
+}
