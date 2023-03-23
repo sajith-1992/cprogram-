e8f953a91a3c3b9e61290bf8997ca90f2c1f7e15
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-23 09:13:44 -0400

    datatype person

---
+++ own data person.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct
+{
+string name;
+string number;
+    
+}
+person;
+int main() {
+   
+person people[2];
+people[0].name="hai";
+people[0].number="495647982";
+people[1].namee="hello";
+people[1].number="154685";
+string get= hello
+   for (int i=0; i<2;i++)
+   {
+       if(strcmp(people[i].name,get)==0)
+       {
+           printf("found %S\n",people[i].number);
+           return 0;
+       
+           
+       }
+   }
+   printf("not found\n");
+   return 1;
+}
---
+++ owndatatype.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct 
+{
+    name;
+    number;
+}
+person;
+
+int main() {
+  
+   for (int i=0; i<3;i++)
+   {
+       if(strcmp(name[i],get)==0)
+       {
+           printf("found %S\n",number[i]);
+       
+           
+       }
+   }
+   printf("not found\n");
+   return 1;
+}
