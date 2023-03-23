af44b8e2d61182461be23e28aaa6f199f479dc8b
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-23 06:44:28 -0400

    phone bok

---
+++ phonebook.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   string name[]={"saji","helo"};
+   string number[]={"647-7469215","456-789-4576"};
+string get="helo"
+   for (int i=0; i<3;i++)
+   {
+       if(strcmp(name[i],get)==0)
+       {
+           printf("found %S\n",number[i]);
+       
+           
+       }
+   }
+   printf("not found\n");
+   return 1;
+}
