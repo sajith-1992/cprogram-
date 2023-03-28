6db2231d4c751f46d0b2b9d62cbd2e3031696e7e
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-28 08:39:14 -0400

    p and  & to show location  adress in memory

---
+++ c spce location.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n =50 ;
+    
+    printf("%p",&n); // p  is place holder & will show the spce 
+
+    return 0;
---
+++ cmpmemory.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n =50 ;
+    
+    printf("%i",n);
+
+    return 0;
+}
