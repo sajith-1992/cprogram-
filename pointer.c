d8ac058cc4ced95e996b1c1a09b3d22ac5c24427
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-30 19:25:55 -0400

    pointer to address

---
+++ pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n =50 ;
+    int *p=&n;
+    printf("%p",p);
+
+    return 0;
+}
---
+++ pointerinside.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n =50 ;
+    int *p=&n;
+    printf("%i",*p);
+
+    return 0;
+}
